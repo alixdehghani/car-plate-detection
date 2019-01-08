0fe3d8fbb279c2c905c6f6a545a5e9011a694404
Ali <alixdehghani@gmail.com>
2019-01-08 13:07:26 +0330

    apply some morphology actions

--- implementation.py
+++ implementation.py
@@ -17,7 +17,7 @@ for img in dir_files:
     im = im.astype('int32')
 
     # plot orginal image
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(1, 6)
 
     # split RGB fron orginal image
     try:
@@ -29,7 +29,7 @@ for img in dir_files:
     gray_im = (3*r+6*g+b)/10
 
     # show gray_im
-    ax1.set_title("Orginal Image")
+    ax1.set_title(img)
     ax1.imshow(gray_im, cmap="gray")
 
     # apply sobel filter in gray_im
@@ -50,7 +50,32 @@ for img in dir_files:
     threshold_value = threshold_otsu(mag)
     binary_car_image = mag > threshold_value
     # show gray_im whith threshold
-    ax3.set_title("Sobel Filtered")
+    ax3.set_title("otsu thresold")
     ax3.imshow(binary_car_image, cmap="gray")
+
+
+    #apply some morphology actions
+
+    #dilation for binary_car_image
+        #dialation make highlight white lines in binary_car_image 
+    kernel = numpy.ones((11,11),numpy.uint8)
+    dilation = cv2.dilate(binary_car_image.astype(float),kernel,iterations = 1)
+    ax4.set_title("dilation")
+    ax4.imshow(dilation, cmap="gray")
+
+
+    #closing for binary_car_image
+        #closing improve white segment in binary_car_image 
+    kernel = numpy.ones((11,11),numpy.uint8)
+    closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+    ax5.set_title("closing")
+    ax5.imshow(closing, cmap="gray")
+
+    #erosion for binary_car_image
+        #erosion is a easy way to remove noise but it is'n a good way 
+    kernel = numpy.ones((35,35),numpy.uint8)
+    erosion = cv2.erode(closing,kernel,iterations = 1)
+    ax6.set_title("erosion")
+    ax6.imshow(erosion, cmap="gray")
 
     plt.show()
